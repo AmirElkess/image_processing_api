af5a833f6ef666c08f92892c1a858e4fc28208df
AmirElkess <amir.elkess@gmail.com>
2022-03-16 20:25:27 +0200

    making modifications according to code review

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -3,16 +3,30 @@ import { getImage, imageNotPresent } from "../../functions";
 
 const images = express.Router();
 
-images.get("/", async (req, res) => {
-  const width = Number(req.query.width);
-  const height = Number(req.query.height);
-  const filename = req.query.filename as string;
-  const response = await getImage(filename, width, height);
-  if (response === imageNotPresent) {
-    res.send("cannot find image specified");
-  } else {
-    res.sendFile(response);
+images.get(
+  "/",
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    const width = Number(req.query.width);
+    const height = Number(req.query.height);
+
+    if (width <= 0 || height <= 0) {
+      res.send("Width and height parameters should be greater than 0");
+      return;
+    }
+
+    if (Number.isNaN(width) || Number.isNaN(height)) {
+      res.send("Width and height parameters should have a numeric value");
+      return;
+    }
+
+    const filename = req.query.filename as string;
+    const response = await getImage(filename, width, height);
+    if (response === imageNotPresent) {
+      res.send("cannot find image specified");
+    } else {
+      res.sendFile(response);
+    }
   }
-});
+);
 
 export default images;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 routes.use("/images", images);
 
-routes.get("/", (req, res) => {
+routes.get("/", (req: express.Request, res: express.Response): void => {
   res.send(
     "navigate to /api/images/ and supply filename, width, and height as  url parameters"
   );
